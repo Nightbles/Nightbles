1ce479aa9164ff6c8ea46d23beabc5af1b7f98e8
Fruncis <93474605+Fruncis@users.noreply.github.com>
2024-04-29 23:29:29 +0200

    Add files via upload

---
+++ Nightbles(native)/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Homepage } from './pagine/homepage';
+import { Card } from './components/homepage/Card';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Opep App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Homepage/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Nightbles(native)/pagine/homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card } from "../components/homepage/Card";
+
+export const Homepage = () => {
+    let data = require("../discoteche.json")
+    console.log(data);
+    const discoteche = data.discoteche;
+    return(
+        <Card nome = {discoteche[0].nome} descrizione = {discoteche[0].descrizione} />
+    )
+    
+}
