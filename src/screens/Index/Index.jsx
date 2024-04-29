cce67ef2b2e162a555feee3ec95c7deed3b11be0
PeppeMalli <167342716+PeppeMalli@users.noreply.github.com>
2024-04-29 23:08:45 +0200

    test cartella

---
+++ src/screens/Index/Index.jsx
@@ -0,0 +1,200 @@
+import React from "react";
+import { Search } from "../../components/Search";
+import { UserIcn1 } from "../../icons/UserIcn1";
+import jsonData from '../Index/discoteche.json';
+import "./style.css";
+
+export const Index = () => {
+  
+  const data = () => JSON.parse(JSON.stringify(jsonData));
+  const discoteche = data.discoteche;
+  const nomi_discoteche = [discoteche];
+  console.log(nomi_discoteche);
+
+  /*fetch('src/screens/discoteche.json')
+    .then((res) => res.text())
+    .then((data) => {
+      JSON.parse(data);
+    })*/
+
+  return (
+    <div className="index">
+      <div className="div">
+        <footer className="footer">
+          <div className="overlap-group-2">
+            <div className="text-wrapper-2">Contatti</div>
+            <div className="rectangle" />
+            <div className="overlap">
+              <img className="img" alt="Instagram" src="/img/instagram.png" />
+            </div>
+            <div className="linked-in-wrapper">
+              <img className="img" alt="Linked in" src="/img/linkedin.png" />
+            </div>
+            <div className="facebook-wrapper">
+              <img className="img-2" alt="Facebook" src="/img/facebook.png" />
+            </div>
+            <div className="tik-tok-wrapper">
+              <img className="img-2" alt="Tik tok" src="/img/tiktok.png" />
+            </div>
+            <div className="you-tube-wrapper">
+              <img className="img-2" alt="You tube" src="/img/youtube.png" />
+            </div>
+            <div className="text-wrapper-3">Our brands</div>
+            <div className="flexcontainer">
+              <p className="text">
+                <span className="span">
+                  andrea@nightbles.com
+                  <br />
+                </span>
+              </p>
+              <p className="text">
+                <span className="span">
+                  giuseppe@nightbles.com
+                  <br />
+                </span>
+              </p>
+              <p className="text">
+                <span className="span">francesco@nightbles.com</span>
+              </p>
+            </div>
+            <div className="text-wrapper-4">Contact us</div>
+            <div className="text-wrapper-5">Privacy Policy</div>
+            <div className="text-wrapper-6">Social</div>
+            <p className="n-nightbles">
+              <span className="text-wrapper-7">N</span>
+              <span className="text-wrapper-8">&nbsp;</span>
+              <span className="text-wrapper-9">| </span>
+              <span className="text-wrapper-10">nightbles</span>
+            </p>
+          </div>
+        </footer>
+        <div className="figmap">
+          <div className="basemap-image-wrapper">
+            <img className="basemap-image" alt="Basemap image" src="/img/basemap-image.png" />
+          </div>
+        </div>
+        <div className="about-us">
+          <div className="text-wrapper-11">Facile</div>
+          <div className="text-wrapper-12">I migliori eventi</div>
+          <div className="text-wrapper-13">I locali piu adatti</div>
+          <div className="text-wrapper-14">Veloce</div>
+          <div className="text-wrapper-15">About us</div>
+          <img className="easy" alt="Easy" src="/img/easy.png" />
+          <img className="checkmark" alt="Checkmark" src="/img/checkmark.png" />
+          <img className="champagne-bottle" alt="Champagne bottle" src="/img/champagne-bottle.png" />
+          <img className="facebook-like" alt="Facebook like" src="/img/facebook-like.png" />
+        </div>
+        <div className="CARD">
+          <div className="overlap-2">
+            <img className="unsplash" alt="Unsplash" src="/img/unsplash-y5pxvs1lpy4-5.png" />
+            <div className="text-wrapper-16">2</div>
+            <p className="p">
+              Lorem ipsum dolor sit amet consectetur. Proin tristique libero tortor tellus pretium magna sollicitudin.
+              Diam feugiat sodales velit feugiat eget eros.
+            </p>
+            <div className="text-wrapper-17">€€€</div>
+          </div>
+        </div>
+        <div className="overlap-wrapper">
+          <div className="overlap-2">
+            <img className="unsplash" alt="Unsplash" src="/img/unsplash-y5pxvs1lpy4-5.png" />
+            <div className="text-wrapper-16">Nome locale</div>
+            <p className="p">
+              Lorem ipsum dolor sit amet consectetur. Proin tristique libero tortor tellus pretium magna sollicitudin.
+              Diam feugiat sodales velit feugiat eget eros.
+            </p>
+            <div className="text-wrapper-17">€€€</div>
+          </div>
+        </div>
+        <div className="overlap-group-wrapper">
+          <div className="overlap-2">
+            <img className="unsplash" alt="Unsplash" src="/img/unsplash-y5pxvs1lpy4-5.png" />
+            <div className="text-wrapper-16">Nome locale</div>
+            <p className="p">
+              Lorem ipsum dolor sit amet consectetur. Proin tristique libero tortor tellus pretium magna sollicitudin.
+              Diam feugiat sodales velit feugiat eget eros.
+            </p>
+            <div className="text-wrapper-17">€€€</div>
+          </div>
+        </div>
+        <div className="div-wrapper">
+          <div className="overlap-2">
+            <img className="unsplash" alt="Unsplash" src="/img/unsplash-y5pxvs1lpy4-5.png" />
+            <div className="text-wrapper-16">Nome locale</div>
+            <p className="p">
+              Lorem ipsum dolor sit amet consectetur. Proin tristique libero tortor tellus pretium magna sollicitudin.
+              Diam feugiat sodales velit feugiat eget eros.
+            </p>
+            <div className="text-wrapper-17">€€€</div>
+          </div>
+        </div>
+        <div className="CARD-2">
+          <div className="overlap-2">
+            <img className="unsplash-ypxvslpy" alt="Unsplash" src="/img/unsplash-y5pxvs1lpy4-5.png" />
+            <div className="text-wrapper-18">Nome locale</div>
+            <p className="text-wrapper-19">
+              Lorem ipsum dolor sit amet consectetur. Proin tristique libero tortor tellus pretium magna sollicitudin.
+              Diam feugiat sodales velit feugiat eget eros.
+            </p>
+            <div className="text-wrapper-20">€€€</div>
+          </div>
+        </div>
+        <div className="CARD-3">
+          <div className="overlap-2">
+            <img className="unsplash" alt="Unsplash" src="/img/unsplash-y5pxvs1lpy4-5.png" />
+            <div className="text-wrapper-16">Nome locale</div>
+            <p className="p">
+              Lorem ipsum dolor sit amet consectetur. Proin tristique libero tortor tellus pretium magna sollicitudin.
+              Diam feugiat sodales velit feugiat eget eros.
+            </p>
+            <div className="text-wrapper-17">€€€</div>
+          </div>
+        </div>
+        <div className="BAR">
+          <div className="overlap-3">
+            <div className="text-wrapper-21">Dove vuoi andare stasera?</div>
+            <Search
+              className="search-instance"
+              frameClassName="search-2"
+              overlapGroupClassName="design-component-instance-node"
+              union="/img/union-1.svg"
+              unionClassName="search-3"
+            />
+          </div>
+        </div>
+        <div className="overlap-4">
+          <div className="int">
+            <div className="overlap-5">
+              <div className="rett" />
+              <p className="n-nightbles-2">
+                <span className="text-wrapper-22">N</span>
+                <span className="text-wrapper-8">&nbsp;</span>
+                <span className="text-wrapper-9">| </span>
+                <span className="text-wrapper-10">nightbles</span>
+              </p>
+              <div className="accedi">
+                <div className="overlap-group-3">
+                  <UserIcn1 className="user-icn" />
+                  <div className="text-wrapper-23">ACCEDI</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="div-2" />
+          <div className="INT">
+            <div className="group">
+              <div className="div-2">
+                <div className="text-wrapper-24">IL TUO LOCALE</div>
+                <img className="line" alt="Line" src="/img/line-5.svg" />
+                <div className="text-wrapper-25">CREA ACCOUNT</div>
+              </div>
+              <div className="text-wrapper-26">ABOUT US</div>
+              <div className="text-wrapper-27">PRENOTA</div>
+              <div className="text-wrapper-28">CONTATTI</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
